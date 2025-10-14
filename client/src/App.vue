d738098147c1bdcd6aec45b7022a96d68724102f
L4P34R <kais.luce@gmail.com>
2025-10-14 19:16:29 +0200

    new project

---
+++ client/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class = "appp">
+    <header>
+
+    </header>
+    <main>
+      <router-view />
+    </main>
+    <footer>
+
+    </footer>
+  </section>
+</template>
+
+<script>
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
+
+<style>
+
+
+</style>
---
+++ client/src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="home">
+        
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
